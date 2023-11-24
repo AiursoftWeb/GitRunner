451a4005808feb274811834114305624f753e837
Anduin Xue <xuef@microsoft.com>
2023-11-24 06:59:43 +0000

    Finish first code.

---
+++ src/Aiursoft.GitRunner/Models/CloneMode.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aiursoft.GitRunner.Models;
+
+public enum CloneMode
+{
+    /// <summary>
+    /// git clone --filter=blob:none <url> 
+    /// 
+    /// creates a blobless clone. These clones download all reachable commits and trees, while fetching blobs on-demand. 
+    /// 
+    /// These clones are best for developers and build environments that span multiple builds.
+    /// </summary>
+    CommitsAndTrees,
+
+    /// <summary>
+    /// git clone --filter=tree:0 <url>
+    /// 
+    /// creates a treeless clone. These clones download all reachable commits, while fetching trees and blobs on-demand. 
+    /// 
+    /// These clones are best for build environments where the repository will be deleted after a single build, but you still need access to commit history.
+    /// </summary>
+    OnlyCommits,
+
+    /// <summary>
+    /// git clone --depth=1 <url>
+    /// 
+    /// creates a shallow clone. These clones truncate the commit history to reduce the clone size. 
+    /// 
+    /// This creates some unexpected behavior issues, limiting which Git commands are possible. These clones also put undue stress on later fetches, so they are strongly discouraged for developer use. They are helpful for some build environments where the repository will be deleted after a single build.
+    /// </summary>
+    Depth1,
+
+    /// <summary>
+    /// git clone <url>
+    /// 
+    /// creates a full clone. These clones download all reachable commits, trees, and blobs.
+    /// 
+    /// These clones are best for developers to build and inspect file history.
+    /// </summary>
+    Full
+}
---
+++ src/Aiursoft.GitRunner/WorkspaceManager.cs
@@ -0,0 +1,241 @@
+﻿using Aiursoft.Canon;
+using Aiursoft.GitRunner.Exceptions;
+using Aiursoft.GitRunner.Models;
+using Aiursoft.GitRunner.Services;
+using Aiursoft.Scanner.Abstractions;
+using Microsoft.Extensions.Logging;
+
+namespace Aiursoft.GitRunner;
+
+/// <summary>
+///     Workspace initializer.
+/// </summary>
+public class WorkspaceManager : ITransientDependency
+{
+    private readonly CommandRunner _commandRunner;
+    private readonly ILogger<WorkspaceManager> _logger;
+    private readonly RetryEngine _retryEngine;
+
+    public WorkspaceManager(
+        ILogger<WorkspaceManager> logger,
+        RetryEngine retryEngine,
+        CommandRunner commandRunner)
+    {
+        _logger = logger;
+        _retryEngine = retryEngine;
+        _commandRunner = commandRunner;
+    }
+
+    /// <summary>
+    ///     Get current branch from a git repo.
+    /// </summary>
+    /// <param name="path">Path</param>
+    /// <returns>Current branch.</returns>
+    public async Task<string> GetBranch(string path)
+    {
+        var gitBranchOutput = await _commandRunner.RunGit(path, "rev-parse --abbrev-ref HEAD");
+        return gitBranchOutput
+            .Split('\n')
+            .Single(s => !string.IsNullOrWhiteSpace(s))
+            .Trim();
+    }
+
+    public async Task SwitchToBranch(string sourcePath, string targetBranch, bool fromCurrent)
+    {
+        var currentBranch = await GetBranch(sourcePath);
+        if (string.Equals(currentBranch, targetBranch, StringComparison.OrdinalIgnoreCase)) return;
+
+        try
+        {
+            await _commandRunner.RunGit(sourcePath, $"checkout -b {targetBranch}");
+        }
+        catch (GitCommandException e) when (e.Message.Contains("already exists"))
+        {
+            if (fromCurrent)
+            {
+                await _commandRunner.RunGit(sourcePath, $"branch -D {targetBranch}");
+                await SwitchToBranch(sourcePath, targetBranch, fromCurrent);
+            }
+            else
+            {
+                await _commandRunner.RunGit(sourcePath, $"checkout {targetBranch}");
+            }
+        }
+    }
+
+    /// <summary>
+    ///     Get remote origin's URL from a local git repo.
+    /// </summary>
+    /// <param name="path">Path.</param>
+    /// <returns>Remote URL.</returns>
+    public async Task<string> GetRemoteUrl(string path)
+    {
+        var gitRemoteOutput = await _commandRunner.RunGit(path, "remote -v");
+        return gitRemoteOutput
+            .Split('\n')
+            .First(t => t.StartsWith("origin"))
+            .Substring(6)
+            .Replace("(fetch)", string.Empty)
+            .Replace("(push)", string.Empty)
+            .Trim();
+    }
+
+    /// <summary>
+    ///     Clone a repo.
+    /// </summary>
+    /// <param name="path">Path on disk.</param>
+    /// <param name="branch">Init branch.</param>
+    /// <param name="endPoint">Endpoint. Used for Git clone.</param>
+    /// <returns>Task</returns>
+    public async Task Clone(string path, string branch, string endPoint, CloneMode cloneMode)
+    {
+        string? command;
+        switch(cloneMode)
+        {
+            case CloneMode.Full:
+                command = $"clone -b {branch} {endPoint} .";break;
+            case CloneMode.OnlyCommits:
+                command = $"clone --filter=tree:0 <url> -b {branch} {endPoint} ."; break;
+            case CloneMode.CommitsAndTrees:
+                command = $"clone --filter=blob:none <url> -b {branch} {endPoint} ."; break;
+            case CloneMode.Depth1:
+                command = $"clone --depth=1 <url> -b {branch} {endPoint} ."; break;
+            default:
+                throw new NotSupportedException($"Clone mode {cloneMode} is not supported.");
+        }
+
+        await _commandRunner.RunGit(path, command);
+    }
+
+    /// <summary>
+    ///     Switch a folder to a target branch (Latest remote).
+    ///     Supports empty folder. We will clone the repo there.
+    ///     Supports folder with existing content. We will clean that folder and checkout to the target branch.
+    /// </summary>
+    /// <param name="path">Path</param>
+    /// <param name="branch">Branch name</param>
+    /// <param name="endPoint">Git clone endpoint.</param>
+    /// <returns>Task</returns>
+    public async Task ResetRepo(string path, string branch, string endPoint, CloneMode cloneMode)
+    {
+        try
+        {
+            var remote = await GetRemoteUrl(path);
+            if (!string.Equals(remote, endPoint, StringComparison.OrdinalIgnoreCase))
+                throw new GitCommandException(
+                    $"The repository with remote: '{remote}' is not a repository for {endPoint}.", "remote -v", remote,
+                    path);
+
+            await _commandRunner.RunGit(path, "reset --hard HEAD");
+            await _commandRunner.RunGit(path, "clean . -fdx");
+            await SwitchToBranch(path, branch, false);
+            await Fetch(path);
+            await _commandRunner.RunGit(path, $"reset --hard origin/{branch}");
+        }
+        catch (GitCommandException e) when (
+            e.Message.Contains("not a git repository") ||
+            e.Message.Contains("unknown revision or path") ||
+            e.Message.Contains($"is not a repository for {endPoint}"))
+        {
+            ClearPath(path);
+            await Clone(path, branch, endPoint, cloneMode);
+        }
+    }
+
+    /// <summary>
+    ///     Do a commit. (With adding local changes)
+    /// </summary>
+    /// <param name="sourcePath">Commit path.</param>
+    /// <param name="message">Commie message.</param>
+    /// <param name="branch">Branch</param>
+    /// <returns>Saved.</returns>
+    public async Task<bool> CommitToBranch(string sourcePath, string message, string branch)
+    {
+        await _commandRunner.RunGit(sourcePath, "add .");
+        await SwitchToBranch(sourcePath, branch, true);
+        var commitResult = await _commandRunner.RunGit(sourcePath, $@"commit -m ""{message}""");
+        return !commitResult.Contains("nothing to commit, working tree clean");
+    }
+
+    public async Task SetUserConfig(string sourcePath, string username, string email)
+    {
+        await _commandRunner.RunGit(sourcePath, $@"config user.name ""{username}""");
+        await _commandRunner.RunGit(sourcePath, $@"config user.email ""{email}""");
+    }
+
+    /// <summary>
+    ///     Push a local folder to remote.
+    /// </summary>
+    /// <param name="sourcePath">Folder path..</param>
+    /// <param name="branch">Remote branch.</param>
+    /// <param name="endpoint">Endpoint</param>
+    /// <param name="force">Force</param>
+    /// <returns>Pushed.</returns>
+    public async Task Push(string sourcePath, string branch, string endpoint, bool force = false)
+    {
+        // Set origin url.
+        try
+        {
+            await _commandRunner.RunGit(sourcePath, $@"remote set-url ninja {endpoint}");
+        }
+        catch (GitCommandException e) when (e.GitOutput.Contains("No such remote"))
+        {
+            await _commandRunner.RunGit(sourcePath, $@"remote add ninja {endpoint}");
+        }
+
+        // Push to that origin.
+        try
+        {
+            var forceString = force ? "--force" : string.Empty;
+
+            var command = $@"push --set-upstream ninja {branch} {forceString}";
+            _logger.LogInformation("Running git {Command}", command);
+            await _commandRunner.RunGit(sourcePath, command);
+        }
+        catch (GitCommandException e) when (e.GitOutput.Contains("rejected]"))
+        {
+            // In this case, the remote branch is later than local.
+            // So we might have some conflict.
+        }
+        catch (Exception ex)
+        {
+            _logger.LogWarning(ex, "Git push failed to {SourcePath}, branch {Branch}, endpoint {Endpoint}", sourcePath, branch, endpoint);
+            throw;
+        }
+    }
+
+    /// <summary>
+    ///     If current path is pending a git commit.
+    /// </summary>
+    /// <param name="sourcePath">Path</param>
+    /// <returns>Bool</returns>
+    public async Task<bool> PendingCommit(string sourcePath)
+    {
+        var statusResult = await _commandRunner.RunGit(sourcePath, @"status");
+        var clean = statusResult.Contains("working tree clean");
+        return !clean;
+    }
+
+    public Task Fetch(string path)
+    {
+        return _retryEngine.RunWithRetry(
+            async attempt =>
+            {
+                var workJob = _commandRunner.RunGit(path, "fetch --verbose",
+                    attempt % 2 == 0);
+                var waitJob = Task.Delay(TimeSpan.FromSeconds(attempt * 50));
+                await Task.WhenAny(workJob, waitJob);
+                if (workJob.IsCompleted)
+                    return await workJob;
+                throw new TimeoutException("Git fetch job has exceeded the timeout and we have to retry it.");
+            });
+    }
+
+    public void ClearPath(string path)
+    {
+        var di = new DirectoryInfo(path);
+        foreach (var file in di.GetFiles()) file.Delete();
+
+        foreach (var dir in di.GetDirectories()) dir.Delete(true);
+    }
+}
